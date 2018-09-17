dcbc63640c8d06d981dedc9f300a8f389505b26d
amolyakov <amolyakov@team.amocrm.com>
2018-09-17 19:55:45 +0300

    asdfasdf asfs

--- tickets/chats.php
+++ tickets/chats.php
@@ -3,6 +3,7 @@ app/routes/api/chats.php
 app/cli/mvc/controllers/chats/messages_controller.php
 app/cli/mvc/tasks/chats/messages.php
 app/libs/chats/amojo/storage/chats.php                               get_by_chat_id
+app/libs/chats/amojo/api/amojo_api_client.php
 
 --------------------------------
 
@@ -11,3 +12,14 @@ frontend/js/lib/components/base/inbox/chats/base_chat.js
 
 --------------------------------
 AMOCRM bitrix/templates/single_page_app/header.php
+
+amojo_chats
+amojo_chats_context
+
+-----------------------------------------------------------------
+'/private/api/v2/json/chats/session?entity=chats&action=session'
+app/libs/mvc/controllers/api/v2/chats/chats_controller.php
+Создание нового токена для пользователя amoJo  function action_session()
+-----------------------------------------------------------------
+'/private/api/v2/json/chats/bots/connect?entity=chats%2Fbots&action=connect'
+app/libs/mvc/controllers/api/v2/chats/bots/bots_controller.php  function action_connect(
